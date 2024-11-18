77d5aaff43a464c36e7d1aa679a8f8e087710dfa
Michael Tomer <Jester3141@gmail.com>
2024-11-17 18:30:46 -0700

    Added in a number of additional experiments.

--- scripts/runEverything.sh
+++ scripts/runEverything.sh
@@ -2,10 +2,22 @@
 set -e
 
 
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.withandwithoutevil.yaml
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaintest.yaml
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gainwithoutoverlaptest.yaml
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaintenmhzbuffertest.yaml
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.arfcntesttest.yaml
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaptest.yaml
-./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddtest.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.withandwithoutevil.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaintest.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gainwithoutoverlaptest.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaintenmhzbuffertest.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.arfcntest.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaptest.yaml
+#./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddtest.yaml
+
+
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gainfivemhzoverlaptest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaintenmhzoverlaptest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.gaintwentymhzoverlaptest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddtenmhzbuffertest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddwithoutoverlaptest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddfivemhzoverlaptest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddtenmhzoverlaptest.yaml
+./launchExperiment.py -u u0204096 -n u0204096-230929 -p TDDInterfere -e experiments.tddtwentymhzoverlaptest.yaml
+
+
